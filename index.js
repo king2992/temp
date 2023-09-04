cea835febf553c39b86dfb287e080942b305ebad
king2992 <king2992@naver.com>
2023-09-04 13:58:41 +0900

    배너 바로 출력

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     if(!getCookie("hideBannerForToday")) {
+        hideBannerForToday();
         var modal = $('#bannerModal');
         modal.show();
 
@@ -35,7 +36,9 @@ function hideBannerForToday() {
     setCookie("hideBannerForToday", true, 1); // 쿠키를 1일 동안 저장
     var modal = document.getElementById('bannerModal');
     modal.style.display = "none";
-    window.location.href = "https://link.coupang.com/a/8IPpw";
+    // window.location.href = "https://link.coupang.com/a/8IPpw";
+    window.open('https://link.coupang.com/a/8IPpw', '_blank');
+
 }
 
 function setCookie(name, value, days) {
